9cc3bc4d9bc0132fc8cc0213b70a5d1a925c4b6e
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-12-11 23:22:45 -0500

    Update message content

--- src/cogs/meme.py
+++ src/cogs/meme.py
@@ -18,7 +18,8 @@ class Meme(commands.Cog):
         self.api_url = "https://api.github.com/repos/cat-milk/Anime-Girls-Holding-Programming-Books/contents"
         self.xkcd_url = "https://xkcd.com/info.0.json"
         self.cute_cat_url = "https://cataas.com/cat/cute"
-        self.random_user_agent = str(random.randint(1, 9)) * random.randint(10, 90)
+        self.random_user_agent = str(
+            random.randint(1, 9)) * random.randint(10, 90)
 
     async def edit_message_with_image(
         self,
@@ -100,7 +101,8 @@ class Meme(commands.Cog):
         language_folder = random.choice(folders)
 
         # Fetching images in the chosen folder
-        language_images = requests.get(f"{self.api_url}/{language_folder}").json()
+        language_images = requests.get(
+            f"{self.api_url}/{language_folder}").json()
 
         # Choosing a random image from the images
         image = random.choice(language_images)
@@ -156,7 +158,8 @@ class Meme(commands.Cog):
 
     @commands.command(brief="Fetch a random image from any subreddit", aliases=["meme"])
     async def reddit(self, ctx: commands.Context, *, subreddit: str = "memes"):
-        fetched_post = requests.get(f"https://meme-api.com/gimme/{subreddit}").json()
+        fetched_post = requests.get(
+            f"https://meme-api.com/gimme/{subreddit}").json()
         if fetched_post.get("message"):
             return await ctx.message.edit(content=fetched_post["message"])
 
@@ -164,7 +167,7 @@ class Meme(commands.Cog):
             ctx=ctx,
             url=fetched_post["url"],
             filename="meme.png",
-            content=f'[Post Link](<{fetched_post["postLink"]}>)\n**{fetched_post["title"]}**',
+            content=f'[**{fetched_post["title"]}**](<{fetched_post["postLink"]}>)',
         )
 
 
